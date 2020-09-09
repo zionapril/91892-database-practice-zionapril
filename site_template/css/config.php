da594046642165d4a87d6a9679993ad67ee54a99
zionapril <61804042+zionapril@users.noreply.github.com>
2020-09-10 09:34:36 +1200

    Update config.php

---
+++ site_template/css/config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'On'); //change to 'Off' when live
+
+define ("DB_NAME","aprilz0456");
+define ("DB_USERNAME","root");
+define ("DB_PASSWORD","");
+define ("DB_HOST","localhost");
+
+?>
