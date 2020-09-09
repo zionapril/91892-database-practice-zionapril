f8a7582915ab30327038ff4567b158cf4e23ddf8
zionapril <61804042+zionapril@users.noreply.github.com>
2020-09-10 09:09:14 +1200

    Initial commit

---
+++ site_template/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+    
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="games, apps, ">
+    <meta name="author" content="your name">
+    <meta name="keywords" content="games, apps, ratings">
+    
+    <title>Game Database</title>
+
+    <!-- for multiple fonts change | to %7c * no spaces*  -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet">  
+
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/data_style.css">    <!-- custom style sheet -->
+
+    
+</head>
+
+<body>
+    
+    <p class="message">Eek!  Your browser does not support grid.  Please upgrade your system.</p>
+        
+    <div class="wrapper">
+    
+        <!-- logo / small image goes here -->
+        <div class="box logo">
+            <a href="index.php"><img src="images/logo.png" width="261" height="150" alt="Dice" /></a>
+        </div> <!-- / logo -->
+        
+        <div class="box banner">
+            <h1>Games Database</h1>        
+        </div> <!-- / banner -->
+                       
+            
+        <div class="box main">
+            <h2>Welcome</h2>
+            
+            
+            <p>
+                Put link to original dataset here
+            </p>
+            
+
+            
+        </div> <!-- / main -->
+        
+        <div class="box side">
+           
+           <h2>Search Area</h2>
+           
+           <p>This is where the search area goes
+
+            
+        </div> <!-- / side bar -->
+        
+        <div class="box footer">
+            CC GTT 20XX
+        </div> <!-- / footer -->
+                
+        
+    </div> <!-- / wrapper -->
+    
+            
+</body>
